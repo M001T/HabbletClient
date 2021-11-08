90c53ed40104abdcbffa8a40d54cc772d06ec747
nick-rangel18 <ceciliaeilish@gmail.com>
2021-11-07 21:39:05 -0300

    Descrição da sua modificação aqui

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let HabbletClient = require("./HabbletClient");
 
-// Nicolas na escuta?
-// Hello
-// Caralho nicolas, que foda!
+//sou lindo
 
 let client = new HabbletClient("edb5289839928000a5e7fdf7a86717eb0a6e119e-36fe32dc8cd73326fdf3b92b9d62794c");
 
